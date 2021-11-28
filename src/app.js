313f8b721dac4ffe6f08e68f4379dbeb235fad2b
Eyvgeniy <antonm33@yandex.ru>
2021-11-28 20:22:30 +0200

    Add errors handler

--- src/app.js
+++ src/app.js
@@ -6,27 +6,30 @@ const {
   updatePerson,
   removePerson,
 } = require('./controllers/personsController');
+const { errorWrapper } = require('./utils');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/persons' && req.method === 'GET') {
-    getPersons(res);
-  } else if (req.url.match(/\/persons\/([a-z0-9-]+)/) && req.method === 'GET') {
-    const id = req.url.split('/')[2];
-    findByIdPerson(res, id);
-  } else if (req.url === '/persons' && req.method === 'POST') {
-    createPerson(req, res);
-  } else if (req.url.match(/\/persons\/([a-z0-9-]+)/) && req.method === 'PUT') {
-    const id = req.url.split('/')[2];
+  errorWrapper(req, res, () => {
+    if (req.url === '/persons' && req.method === 'GET') {
+      getPersons(res);
+    } else if (req.url.match(personRouteRegExp) && req.method === 'GET') {
+      const id = req.url.split('/')[2];
+      findByIdPerson(res, id);
+    } else if (req.url === '/persons' && req.method === 'POST') {
+      createPerson(req, res);
+    } else if (req.url.match(personRouteRegExp) && req.method === 'PUT') {
+      const id = req.url.split('/')[2];
 
-    updatePerson(req, res, id);
-  } else if (req.url.match(/\/persons\/([a-z0-9-]+)/) && req.method === 'DELETE') {
-    const id = req.url.split('/')[2];
+      updatePerson(req, res, id);
+    } else if (req.url.match(personRouteRegExp) && req.method === 'DELETE') {
+      const id = req.url.split('/')[2];
 
-    removePerson(req, res, id);
-  } else {
-    res.writeHead(404);
-    res.end('The route doesn`t exist');
-  }
+      removePerson(req, res, id);
+    } else {
+      res.writeHead(404);
+      res.end('The route doesn`t exist');
+    }
+  });
 });
 
 module.exports = server;
--- src/utils/index.js
+++ src/utils/index.js
@@ -16,13 +16,27 @@ const getBody = (req) =>
 
 const uuidRegExp = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89AB][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
+const personRouteRegExp = /\/persons\/([a-z0-9-]+)/;
+
 const sendWrongRequest = (res, error = 'Wrong request') => {
   res.writeHead(400, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ message: error }));
+};
+
+const errorWrapper = (res, req, func) => {
+  try {
+    func();
+  } catch (error) {
+    console.error(error);
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Something went wrong' }));
+  }
 };
 
 module.exports = {
   getBody,
   uuidRegExp,
   sendWrongRequest,
+  errorWrapper,
+  personRouteRegExp,
 };
